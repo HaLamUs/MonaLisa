43154a6633fac04128170651d2cb1a9001e3d65f
Ha Lam <halam@LamHa.local>
2016-10-31 21:11:38 +0700

    Init commit

---
+++ Siclo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Siclo
+//
+//  Created by Ha Lam on 10/31/16.
+//  Copyright © 2016 Siclo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
